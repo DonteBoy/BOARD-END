cf49ada65870a8c718e539ca313353a0338ae277
Donte <70739348+DonteBoy@users.noreply.github.com>
2022-03-24 23:25:52 +0200

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,17 @@
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+} 
+
+var toastTrigger = document.getElementById('btn_White')
+
+var toastTrigger = document.getElementById('btn_Auto')
+
+var toastTrigger = document.getElementById('btn_Black')
+
+
